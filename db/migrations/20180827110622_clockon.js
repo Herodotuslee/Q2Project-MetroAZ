dfb7591be46e542c58795ca97dff8934d3afd634
bonjouralbertlee@gmail.com <herodotuslee@gmail.com>
2018-08-27 13:57:12 -0700

    clock_on

--- db/migrations/20180827110622_clockon.js
+++ db/migrations/20180827110622_clockon.js
@@ -1,16 +1,19 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("clockon",(table)=>{
+  return knex.schema.createTable("clock_on",(table)=>{
     table.increments();
     table.integer("employee_id")
     .references("id")
     .inTable("employee")
     .onDelete("CASCADE")
     .index();
+    table.string("clock_in_out")
+    table.date('clockin_date').defaultTo(knex.fn.now(6));
+    table.time('clockin_time').defaultTo(knex.fn.now(6));
     table.timestamps(true,true);
   })
 
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("clockon");
+  return knex.schema.dropTable("clock_on");
 };
