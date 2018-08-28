09db673c788cd756a2a44062db4453c1442a244d
bonjouralbertlee@gmail.com <herodotuslee@gmail.com>
2018-08-28 16:44:23 -0700

    logclockin

--- controllers/admin.js
+++ controllers/admin.js
@@ -44,6 +44,19 @@ module.exports = {
       })
   },
   timelog:(req,res)=>{
-      res.render('timelog')
-  }
+    knex('employee')
+    .leftJoin('clock','employee.id','employee_id')
+    .then((result)=>{
+      // res.send(result)
+      res.render('timelog',{result})
+    })
+  },
+  // showTimeLog:(req,res)=>{
+  //   knex('employee').where('name','Albert')
+  //   .join('clock','employee_id',1)
+  //   .then((result)=>{
+  //     res.render('timelog',{result})
+  //   })
+  //
+  // }
 }
