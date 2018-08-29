6d7e4a6bee9c13b56f32e7662fd5ba1ff8b02c2c
Jake Day <jakeday@Jakes-MBP.domain>
2018-08-28 20:04:09 -0700

    google maps api

---
+++ maps/map.js
@@ -0,0 +1,10 @@
+function initMap() {
+  let location = {
+    lat: 33.5506417,
+    long: -112.2092945
+  };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location
+  });
+}
