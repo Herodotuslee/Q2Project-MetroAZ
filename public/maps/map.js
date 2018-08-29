41717f3d985a143cb195a4d229ea321cd03bf968
Jake Day <jakeday@Jakes-MacBook-Pro.local>
2018-08-29 15:53:07 -0700

    map updated

--- public/maps/map.js
+++ public/maps/map.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:  33.5506517, lng: -112.2093833},
-    zoom: 15
+    zoom: 18
   });
 }
