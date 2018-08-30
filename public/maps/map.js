e1df78b8eb3527f23dd9890f37ca086676a463b2
Jake Day <jakeday@Jakes-MacBook-Pro.local>
2018-08-29 18:06:25 -0700

    updated google maps

--- public/maps/map.js
+++ public/maps/map.js
@@ -1,8 +1,18 @@
-
 let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:  33.5506517, lng: -112.2093833},
-    zoom: 18
+    zoom: 17
+  });
+
+  let officeimg = {
+    url: "https://cdn2.iconfinder.com/data/icons/gur-project-1/32/1_29.png",
+    scale: 0
+  };
+
+  let marker= new google.maps.Marker({
+    position: {lat:  33.5506517, lng: -112.2093833},
+    map: map,
+    icon:officeimg
   });
 }
